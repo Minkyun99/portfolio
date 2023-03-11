72c0ad059192b2be90f2ee81e4c59fc3bface893
Minkyun99 <alsrud7412@naver.com>
2023-03-12 00:07:37 +0900

    230312

---
+++ portfolio/mdb.cjs
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+const Vschemas = require('./schema.cjs')
+const Counter_Schema = require('./Counter_Schema.cjs')
+const USER = 'mk'
+const PWD = 'mk1234'
+const HOST = '127.0.0.1:27017' //localhost접속이 불가
+const DB = 'admin'
+
+const mongodbURL = `mongodb://${USER}:${PWD}@${HOST}/${DB}`
+
+//mongoose.set('useFindAndModify',false)    //버전 6부터 자동관리
+mongoose.set('strictQuery', false) //변경사항 넣기
+mongoose
+  .connect(mongodbURL, { useNewUrlParser: true })
+  .then(() =>
+    console.log(
+      'MDB Connection Successful\nSchema load successful\nQuery Loading'
+    )
+  )
+  .catch((e) => console.error(e))
+module.exports = Vschemas
+module.exports = Counter_Schema
---
+++ portfolio/schema.cjs
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const VSchema = new Schema({
+  name: String,
+  id: String,
+  pwd: String,
+  gender: String,
+  age: String,
+  hobby: String,
+  PhoneNumber: String,
+  identification: String
+})
+
+module.exports = mongoose.model('vdb', VSchema, 'vdb')
